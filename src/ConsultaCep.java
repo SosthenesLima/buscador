ba51aaf095961927afe7e1a9954c8df3664435a3
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-07-08 21:51:35 -0300

    Commit Obtendo um endereço da API ViaCEP04

--- src/ConsultaCep.java
+++ src/ConsultaCep.java
@@ -1,6 +1,18 @@
+/*
+  By Sósthenes Lima
+ */
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
 public class ConsultaCep {
 
     public Endereco buscaEndereco(String cep) {
+        URI endereco = "viacep.com.br/ws/" + cep + "/jason";
 
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(endereco)
+                .build();
     }
 }
