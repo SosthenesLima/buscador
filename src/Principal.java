2ba71b53e3be434878f63b522b01cf1fe4025b13
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2024-07-13 21:20:21 -0300

    Commit Obtendo um endereço da API ViaCEP10

--- src/Principal.java
+++ src/Principal.java
@@ -6,6 +6,9 @@
 public class Principal {
     public static void main(String[] args) {
 
+        ConsultaCep consultaCep = new ConsultaCep();
+        Endereco novoEndereco = consultaCep.buscaEndereco("01001000");
+        System.out.println(novoEndereco);
     }
 
 
